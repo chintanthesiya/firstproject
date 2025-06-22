bc15739923e133a082a2effbd09d0c205694b1e2
chintanthesiya <chintanthesiya0142@gmail.com>
2025-06-22 23:10:15 +0530

    add voice and file generater

--- news_generater.py
+++ news_generater.py
@@ -1,5 +1,7 @@
 # generate news and fake and funny and print there project using Python
 import random
+import pyttsx3
+
 
 subjects=[
     "shahrukh khan",
@@ -25,20 +27,26 @@ place_or_things=["at red fort",
     "at ganga ghat",
     "suring ipl match",
     "india gate"]
-
-while True:
-    subject=random.choice(subjects)
-    action=random.choice(actions)
-    place=random.choice(place_or_things)
-    headline=f" BREAKING NEWS {subject} {action} {place}"
-
-    print("\n ",headline)
-    user=input("do you want anoter headline ? (yes/no)").strip().lower() # strip is remove spaces in user input
-
-    if user=="no":
-        break
+with open("fake_news_log.txt", "a") as file:
+    while True:
+        subject=random.choice(subjects)
+        action=random.choice(actions)
+        place=random.choice(place_or_things)
+        headline=f" BREAKING NEWS {subject} {action} {place}"
+        engine = pyttsx3.init()
+        engine.say(headline)
+        engine.runAndWait()
+   
+        print("\n ",headline)
+        file.write(headline + "\n")
+        user=input("do you want anoter headline ? (yes/no)").strip().lower() # strip is remove spaces in user input
+       
+        if user=="no":
+            break
 
 print("\n thanks for using the fake news headline generator. have a fun day")
+
+  
 
 
 
