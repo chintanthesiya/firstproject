3a2d3b295f426343694b6811fefa6763c25e8655
chintanthesiya <chintanthesiya0142@gmail.com>
2025-06-22 23:43:01 +0530

    fix if condition

--- news_generater.py
+++ news_generater.py
@@ -2,7 +2,13 @@
 import random
 import pyttsx3
 
-
+categories = {
+    "Politics": ["Nirmala Sitharaman", "Prime Minister Modi"],
+    "Bollywood": ["Shahrukh Khan"],
+    "Sports": ["Virat Kohli"],
+    "Animals": ["a Mumbai cat", "a group of monkeys"],
+    "Public": ["an auto-rickshaw driver from Delhi"]
+}
 subjects=[
     "shahrukh khan",
     "virat kohli",
@@ -29,19 +35,22 @@ place_or_things=["at red fort",
     "india gate"]
 with open("fake_news_log.txt", "a") as file:
     while True:
+        category=random.choice(list(categories.keys()))
         subject=random.choice(subjects)
         action=random.choice(actions)
         place=random.choice(place_or_things)
-        headline=f" BREAKING NEWS {subject} {action} {place}"
-        engine = pyttsx3.init()
-        engine.say(headline)
-        engine.runAndWait()
+        headline=f"{category} : BREAKING NEWS {subject} {action} {place}"
+        
    
         print("\n ",headline)
+        engine = pyttsx3.init()
+        voices = engine.getProperty('voices') 
+        engine.say(headline)
+        engine.runAndWait()
         file.write(headline + "\n")
         user=input("do you want anoter headline ? (yes/no)").strip().lower() # strip is remove spaces in user input
        
-        if user=="no":
+        if user!="yes":
             break
 
 print("\n thanks for using the fake news headline generator. have a fun day")
